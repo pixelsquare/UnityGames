8c1b349f3a7fb70c84821b6852c61b66c5a75217
Anthony Ganzon <anthony_0205@yahoo.com>
2015-10-12 20:56:30 +0800

    Added Astaris Adventure utils

---
+++ Assets/Utils/EventBroadcaster.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+
+public class EventBroadcaster {
+	// Private Variables
+	private Dictionary<string, ObjectListener> objListener;
+
+	private static EventBroadcaster instance;
+	public static EventBroadcaster sharedInstance {
+		get {
+			if (instance == null) {
+				instance = new EventBroadcaster();
+			}
+
+			return instance;
+		}
+	}
+
+	private EventBroadcaster() {
+		objListener = new Dictionary<string, ObjectListener>();
+	}
+
+	public void AddObserver(string name, System.Action action) {
+		ObjectListener listener = null;
+		if (objListener.ContainsKey(name)) {
+			listener = objListener[name];
+			listener.AddObserver(action);
+		}
+		else {
+			listener = new ObjectListener();
+			listener.AddObserver(action);
+			objListener.Add(name, listener);
+		}
+	}
+
+	public void AddObserver(string name, System.Action<Parameters> action) {
+		ObjectListener listener = null;
+		if (objListener.ContainsKey(name)) {
+			listener = objListener[name];
+			listener.AddObserver(action);
+		}
+		else {
+			listener = new ObjectListener();
+			listener.AddObserver(action);
+			objListener.Add(name, listener);
+		}
+	}
+
+	public void RemoveObserverAction(string name, System.Action action) {
+		if (objListener.ContainsKey(name)) {
+			ObjectListener listener = objListener[name];
+			listener.RemoveObserver(action);
+		}
+	}
+
+	public void RemoveObserverAction(string name, System.Action<Parameters> action) {
+		if (objListener.ContainsKey(name)) {
+			ObjectListener listener = objListener[name];
+			listener.RemoveObserver(action);
+		}
+	}
+
+	public void CallObserver(string name) {
+		if (objListener.ContainsKey(name)) {
+			ObjectListener listener = objListener[name];
+			listener.NotifyObservers();
+		}
+	}
+
+	public void CallObserver(string name, Parameters param) {
+		if (objListener.ContainsKey(name)) {
+			ObjectListener listener = objListener[name];
+			listener.NotifyObservers(param);
+		}
+	}
+
+	public void RemoveObserver(string name) {
+		if (objListener.ContainsKey(name)) {
+			ObjectListener listener = objListener[name];
+			listener.RemoveAllObservers();
+			objListener.Remove(name);
+		}
+	}
+
+	public void ClearObservers() {
+		foreach (ObjectListener listener in objListener.Values) {
+			listener.RemoveAllObservers();
+		}
+
+		objListener.Clear();
+	}
+}
---
+++ Assets/Utils/Parameters.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Parameters {
+
+	// Public Variables
+
+	// Private Variables
+	private Dictionary<string, char> charParam;
+	private Dictionary<string, int> intParam;
+	private Dictionary<string, bool> boolParam;
+	private Dictionary<string, string> stringParam;
+	private Dictionary<string, double> doubleParam;
+	private Dictionary<string, float> floatParam;
+
+	private Dictionary<string, ArrayList> arrayListParam;
+	private Dictionary<string, object> objectParam;
+
+	// Static Variables
+
+	public Parameters() {
+		charParam = new Dictionary<string, char>();
+		intParam = new Dictionary<string, int>();
+		boolParam = new Dictionary<string, bool>();
+		stringParam = new Dictionary<string, string>();
+		doubleParam = new Dictionary<string, double>();
+		floatParam = new Dictionary<string, float>();
+
+		arrayListParam = new Dictionary<string,ArrayList>();
+		objectParam = new Dictionary<string,object>();
+	}
+
+	public void PutExtra(string name, char value) {
+		charParam.Add(name, value);
+	}
+
+	public void PutExtra(string name, int value) {
+		intParam.Add(name, value);
+	}
+
+	public void PutExtra(string name, bool value) {
+		boolParam.Add(name, value);
+	}
+
+	public void PutExtra(string name, string value) {
+		stringParam.Add(name, value);
+	}
+
+	public void PutExtra(string name, double value) {
+		doubleParam.Add(name, value);
+	}
+
+	public void PutExtra(string name, float value) {
+		floatParam.Add(name, value);
+	}
+
+	public void PutExtra(string name, ArrayList value) {
+		arrayListParam.Add(name, value);
+	}
+
+	public void PutExtra(string name, object value) {
+		objectParam.Add(name, value);
+	}
+
+	public char GetExtra(string name, char defaultValue) {
+		return (charParam.ContainsKey(name)) ? charParam[name] : defaultValue;
+	}
+
+	public int GetExtra(string name, int defaultValue) {
+		return (intParam.ContainsKey(name)) ? intParam[name] : defaultValue;
+	}
+
+	public bool GetExtra(string name, bool defaultValue) {
+		return (boolParam.ContainsKey(name)) ? boolParam[name] : defaultValue;
+	}
+
+	public string GetExtra(string name, string defaultValue) {
+		return (stringParam.ContainsKey(name)) ? stringParam[name] : defaultValue;
+	}
+
+	public double GetExtra(string name, double defaultValue) {
+		return (doubleParam.ContainsKey(name)) ? doubleParam[name] : defaultValue;
+	}
+
+	public float GetExtra(string name, float defaultValue) {
+		return (floatParam.ContainsKey(name)) ? floatParam[name] : defaultValue;
+	}
+
+	public ArrayList GetExtra(string name, ArrayList defaultValue) {
+		return (arrayListParam.ContainsKey(name)) ? arrayListParam[name] : defaultValue;
+	}
+
+	public object GetExtra(string name, object defaultValue) {
+		return (objectParam.ContainsKey(name)) ? objectParam[name] : defaultValue;
+	}
+}
