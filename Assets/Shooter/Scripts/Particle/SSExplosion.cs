4a1639dbc488592028c0f238bae7a4e3e983990e
Anthony Ganzon <anthony_0205@yahoo.com>
2015-10-14 16:14:41 +0800

    Added particle scripts

---
+++ Assets/Shooter/Scripts/Particle/SSExplosion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SSExplosion : MonoBehaviour {
+	[SerializeField]
+	private ParticleSystem particle;
+
+	public void OnEnable() {
+		StartCoroutine(DisableParticle());
+	}
+
+	public IEnumerator DisableParticle() {
+		while (!particle.isStopped) {
+			yield return null;
+		}
+
+		gameObject.SetActive(false);
+	}
+}
