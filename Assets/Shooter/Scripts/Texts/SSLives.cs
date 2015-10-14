2238d648a16e529ab2a507f1cd883a6d96c3d9a2
Anthony Ganzon <anthony_0205@yahoo.com>
2015-10-14 16:13:43 +0800

    Added texts scripts

---
+++ Assets/Shooter/Scripts/Texts/SSLives.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SSLives : MonoBehaviour {
+
+	private int lives;
+	private Text livesText;
+
+	private const string LIVES_FORMAT = "Lives: {0}";
+
+	public void OnEnable() {
+		EventBroadcaster.sharedInstance.AddObserver(SSNames.ON_TITLE_CLICKED, OnTitleClicked);
+		EventBroadcaster.sharedInstance.AddObserver(SSNames.ON_LIVES_CHANGED, OnLivesChanged);
+	}
+
+	public void OnDisable() {
+		EventBroadcaster.sharedInstance.RemoveObserverAction(SSNames.ON_TITLE_CLICKED, OnTitleClicked);
+		EventBroadcaster.sharedInstance.RemoveObserverAction(SSNames.ON_LIVES_CHANGED, OnLivesChanged);
+	}
+
+	public void OnTitleClicked() {
+		lives = SSGameSettings.PLAYER_LIVES;
+		livesText = GetComponent<Text>();
+		SetLivesDirty();
+	}
+
+	public void OnLivesChanged() {
+		lives -= 1;
+		SetLivesDirty();
+	}
+
+	public void SetLivesDirty() {
+		livesText.text = string.Format(LIVES_FORMAT, lives);
+	}
+}
---
+++ Assets/Shooter/Scripts/Texts/SSScore.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SSScore : MonoBehaviour {
+
+	private int score;
+	private Text scoreText;
+
+	private const string SCORE_FORMAT = "Score: {0:000000}";
+
+	public void OnEnable() {
+		EventBroadcaster.sharedInstance.AddObserver(SSNames.ON_TITLE_CLICKED, OnTitleClicked);
+		EventBroadcaster.sharedInstance.AddObserver(SSNames.ON_SCORE_CHANGED, OnScoreChanged);
+		EventBroadcaster.sharedInstance.AddObserver(SSNames.ON_GAME_OVER, OnGameOver, true);
+	}
+
+	public void OnDisable() {
+		EventBroadcaster.sharedInstance.RemoveObserverAction(SSNames.ON_TITLE_CLICKED, OnTitleClicked);
+		EventBroadcaster.sharedInstance.RemoveObserverAction(SSNames.ON_SCORE_CHANGED, OnScoreChanged);
+		EventBroadcaster.sharedInstance.RemoveObserverAction(SSNames.ON_GAME_OVER, OnGameOver);
+	}
+
+	public void OnTitleClicked() {
+		score = 0;
+		scoreText = GetComponent<Text>();
+		SetScoreDirty();
+	}
+
+	public void OnScoreChanged(Parameters param) {
+		score += param.GetExtra(SSNames.ADDITIONAL_SCORE, 0);
+		SetScoreDirty();
+	}
+
+	public void SetScoreDirty() {
+		scoreText.text = string.Format(SCORE_FORMAT, score);
+	}
+
+	public void OnGameOver() {
+		Parameters param = new Parameters();
+		param.PutExtra(SSNames.FINAL_SCORE, score);
+		EventBroadcaster.sharedInstance.NotifyObserver(SSNames.ON_FINAL_SCORE_CHANGED, param);
+	}
+}
